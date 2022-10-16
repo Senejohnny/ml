cbaa93a5247fac0c0c3cad43011d1c124df00244
Danial <46810505+Senejohnny@users.noreply.github.com>
2022-10-16 11:54:25 +0200

    first push

---
+++ Notebooks/ABN/metadata.py
@@ -0,0 +1,37 @@
+""" this file contains the meta date as to ABN AMRO churn prediction project """
+
+# Churn on which type of product package are we talkng about?
+# I like meta data for a qucik sneak peek at the data & data validation & data missingness
+
+data_type = {
+    # 'MONTH_PERIOD': 'str',          # 24 Months record, values months 
+    'CHURNED_IND': 'int8',            # Client churned or not, values 0 & 1
+    'COMMERCIALLY_CHURNED': 'int8',   # Differ churn in   or not, values 0 & 1
+    'PAYMENT_IND': 'int8',            # Client has payed, values 0 & 1
+    'SAVING_IND': 'int8',             # Client has savings at bank, 0 & 1
+    'INVESTMENTS_IND': 'int8',        # Client has invetment records, 0 & 1
+    'LENDING_IND': 'int8',            # Client has lending records, 0 & 1
+    'INSURANCE_LIFE_IND': 'int8',     # Client has life insurance records, 0 & 1
+    'INSURANCE_NONLIFE_IND': 'int8',  # Client has non life insurance records, 0 & 1
+    'MORTGAGE_IND': 'int8',           # Client has mortgage, 0 & 1
+    'CROSS_SELL_SCORE': 'int8',       # Client score in terms of response to cross sell attempt, values 0 to 7
+    'PACKAGE_IND': 'int8',            # Client subscripe to cetrain package, 0 & 1
+    'CREDIT_CLASS': 'int8',           # Client assigned credit class, values 0 to 8
+    'DEBIT_CLASS': 'int8',            # Client assigned debit class, values 0 to 5
+    'BUSINESS_VOLUME_CLASS': 'int8',  # Client business volumne class, values 0 to 9
+    'BUSINESS_VOLUME_CLASS': 'int8',  # Client business volumne class, values 0 to 9
+    'INVESTED_CAPITAL_CLASS': 'int8', # Client invested capital class, values 0 to 9
+    'SAVINGS_CAPITAL_CLASS': 'int8',  # Client invested capital class, values 0 to 10
+    'MIN_FEED_CLASS': 'int8',         # Client minimum feed class, 0 to 6
+    'REVENUES_CLASS': 'int8',         # Client revenue class, values 0 to 19
+    'PAYMENT_ACTIVITIES_CODE': 'int8',# Client paymeny activity code, values 0 to 4
+    'CLIENTGROUP': 'category',        # Group to which client belongs, 48 unique groups
+    'ACCOUNTMODEL': 'category',       # Client account model, acceptable categories {LP, HW, MP, HP}, [Why missingness?]
+    'AGE_CLASS': 'category',          # Client age groups,  [Why missingness?], unknown
+                                      # [12, 17], [18, 23], [24-29], [30-34], [35-40], [41-54], [55-64], [65-74], [75+]
+    'HOMEBANK_COLOUR': 'category',     # Client home bank colour, 'Geel', 'Groen', 'Oranje', 'Rood', missing value exist. [Why missingness?]
+    'LOYALITY': 'category',           # Client loyalty colour, 'Oranje', 'Groen', 'Rood', 'Wit', [Why missingness?]
+    'TARGET': 'category',             # Client Target, constant category 99
+    'Record_Count': 'category',       # Number of records available per client, constant 24
+    'CUSTOMER_ID': 'category',        # Client ID 
+}
---
+++ src/pipe_store.py
@@ -0,0 +1,63 @@
+""" This is a store of all the relevant functions consumed by pandas pipeline """
+import logging
+from datetime import datetime 
+from functools import wraps
+import pandas as pd 
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
+
+def logging(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        tic = datetime.now()
+        result = func(*args, **kwargs)
+        time_taken = str((datetime.now() - tic).total_seconds())
+        logger.info(f"Step: {func.__name__} | Shape: {result.shape} | Computation Time: {time_taken}s")
+        return result
+    return wrapper
+
+@logging
+def set_dtypes(df:pd.DataFrame, data_type:dict) -> pd.DataFrame:
+    """ Setting data types to a  """
+    for col in data_type.keys():
+        if col not in df.columns:
+            raise KeyError(f'Column {col} not exist')
+
+    for col, dtype in data_type.items():
+        try:
+            df[col] = df[col].astype(dtype)
+        except:
+            print(f'Column {col} encounters error')
+            raise
+    return df
+
+
+def sklearn_adapter(df:pd.DataFrame, label:str):
+    """ Splits the data frame to input matrix and output vector suitable for sklearn package adapter """
+    _df = df.copy(deep=True)
+    return (_df.drop(label, axis=1), _df.pop(label))
+
+
+
+def plot_correlations(df, verbose=True, **kwargs):
+    """ If verbose variables with correlation above 0.6 are printed """
+    
+    # Correlation matrix for numerical variables
+    corr_mat = df.corr()
+    plt.figure(figsize=(10, 6))
+    heatmap(corr_mat, cmap='Blues', annot=True, **kwargs)
+    plt.show()
+
+    if verbose:
+        top_corr = (
+            corr_mat
+            .abs()
+            .where(np.triu(np.ones(corr_mat.shape), k=1).astype(bool))
+            .stack()
+            .sort_values(ascending=False)
+        )
+        print('Top Correlations above 0.6')
+        print(top_corr[top_corr > 0.6])
+
